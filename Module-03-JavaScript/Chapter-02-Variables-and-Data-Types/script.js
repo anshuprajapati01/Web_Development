6b6772e7467c86c60db2f9282ae6d10834b77d15
Anshu Prajapati <anshuparajapti61@gmail.com>
2026-08-20 21:47:56 +0530

    Complete JavaScript Chapter-02: Variables and Data Types

---
+++ Module-03-JavaScript/Chapter-02-Variables-and-Data-Types/script.js
@@ -0,0 +1,8 @@
+let age = 21;
+let name = "Anshu";
+let isStudent = true;
+let studentName = "Anshu";
+let collegeName = "BIT";
+let totalMarks = 450;
+console.log(age,name,isStudent,studentName,collegeName,totalMarks,);
+console.log(typeof isStudent);
