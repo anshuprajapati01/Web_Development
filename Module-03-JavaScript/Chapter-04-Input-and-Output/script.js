338567835b216cf130bc42d0f80a9882f72b3f7c
Anshu Prajapati <anshuparajapti61@gmail.com>
2026-08-20 23:23:26 +0530

    Complete JavaScript Chapter-04: Input and Output

---
+++ Module-03-JavaScript/Chapter-04-Input-and-Output/script.js
@@ -0,0 +1,11 @@
+let a = Number(prompt("Enter first number:"));
+let b = Number(prompt("Enter Second number:"));
+
+console.log(`First number is ${a} and Second number is ${b}`);
+
+console.log("Addition", a + b);
+console.log("Subtraction", a - b);
+console.log("Multiplication", a * b);
+console.log("Division", a / b);
+console.log("Modulo", a % b);
+console.log("Power", a ** b);
