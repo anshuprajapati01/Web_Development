f67897ac8e9f15ac1b9153ba85054fc3ce40774a
Anshu Prajapati <anshuparajapti61@gmail.com>
2026-08-21 00:49:31 +0530

    Complete JavaScript Chapter-07: Strings

---
+++ Module-03-JavaScript/Chapter-07-Strings/script.js
@@ -0,0 +1,15 @@
+let skills = "HTML,CSS,JavaScript";
+
+let result = skills.split(",");
+
+console.log(result);
+
+let a = "I like Java";
+
+let op = a.replace("Java", "JavaScript");
+
+console.log(op);
+
+let text = "Java Java Java";
+
+console.log(text.replaceAll("Java", "Python"));
