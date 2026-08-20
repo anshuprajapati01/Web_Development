cc84bd79a9ef0bee0f9ba8fb9671dcd0457dc2a2
Anshu Prajapati <anshuparajapti61@gmail.com>
2026-08-20 23:38:24 +0530

    Complete JavaScript Chapter-06: Loops

---
+++ Module-03-JavaScript/Chapter-06-Loops/script.js
@@ -0,0 +1,11 @@
+// Loop through numbers from 1 to 20
+for (let i = 1; i <= 20; i++) {
+    // If the remainder is 0 when divided by 2, it is even
+    if (i % 2 === 0) {
+        console.log(`${i} is an Even number`);
+    } 
+    // Otherwise, the number is odd
+    else {
+        console.log(`${i} is an Odd number`);
+    }
+}
