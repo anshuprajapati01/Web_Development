dfea0480befc76c7986f42490a2f18753ca6ba32
Anshu Prajapati <anshuparajapti61@gmail.com>
2026-08-20 22:16:23 +0530

    Complete JavaScript Chapter-03: Operators

---
+++ Module-03-JavaScript/Chapter-03-Operators/script.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 2;
+console.log("Addition",a+b);
+console.log("Substraction",a-b);
+console.log("Multiplication",a*b);
+console.log("Division",a/b);
+console.log("Modulo",a%b);
+console.log("Power",a**b);
